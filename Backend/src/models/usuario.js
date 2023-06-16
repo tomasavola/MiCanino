c9b8461721c97b967fe0141a63173314b4c58b11
TOMAS AGUSTIN AVOLA <47297744@ort>
2023-06-16 11:58:12 -0300

    avanzando usuario

---
+++ Backend/src/models/usuario.js
@@ -0,0 +1,11 @@
+export default class Usuario{
+    constructor(id, nombre, apellido, mail,telefono,password){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.telefono = telefono;
+        this.password = password;
+    }
+    
+}
---
+++ Backend/src/services/usuario-services.js
@@ -0,0 +1,4 @@
+import config from "../../dbconfig.js";
+import sql from 'mssql';
+import CopiaError from "../modules/log-helper.js";
+import Pizza from "../models/usuario.js";
