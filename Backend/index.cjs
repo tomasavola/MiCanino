01339971f0705d7938e427aefe7c012096aa0db8
IVAN KWIATKOWSKI <46645772@ort>
2023-07-07 09:44:09 -0300

    vamos a hacer el login

--- Backend/index.cjs
+++ Backend/index.cjs
@@ -1,66 +1,54 @@
-// npm run server
 import express from "express";
-import cors    from "cors";
+import cors from "cors";
 import CaninoRouter from "./src/controllers/caninoController.js";
 import UsuarioRouter from "./src/controllers/usuarioController.js";
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json({ limit: '10mb' }))
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-//
-// Variables/Constantes del Modulo
-//
-const app  = express();
-const port = 5000;                  // Puerto en donde levanta express (5000)
-//const caninoService = new CaninoService();
+const app = express();
+const port = 5000;
 
-//
-// Inclusion de los Middlewares
-//
-app.use(cors());                    // agrego el middleware de CORS
-app.use(express.json());            // agrego el middleware para parsear y comprender JSON
-app.use(express.static('public'));  // agrego el middleware de retornar archivos estaticos
-app.use('/api/caninos/', CaninoRouter); 
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use('/api/caninos/', CaninoRouter);
 app.use('/api/caninos/usuario/', UsuarioRouter);
 
 const credentials = {
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'samus'
-}
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'samus'
+};
 
 app.get('/', (req, res) => {
-	res.send('hola desde tu primera ruta de la Api')
-})
+  res.send('hola desde tu primera ruta de la Api');
+});
 
 app.post('/api/login', (req, res) => {
-	const { username, password } = req.body
-	const values = [username, password]
-	var connection = mysql.createConnection(credentials)
-	connection.query("SELECT * FROM login WHERE username = ? AND password = ?", values, (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			if (result.length > 0) {
-				res.status(200).send({
-					"id": result[0].id,
-					"user": result[0].user,
-					"username": result[0].username
-				})
-			} else {
-				res.status(400).send('Usuario no existe')
-			}
-		}
-	})
-	connection.end()
-})
+  const { username, password } = req.body;
+  const values = [username, password];
+  var connection = mysql.createConnection(credentials);
+  connection.query("SELECT * FROM login WHERE username = ? AND password = ?", values, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      if (result.length > 0) {
+        res.status(200).send({
+          "id": result[0].id,
+          "user": result[0].user,
+          "username": result[0].username
+        });
+      } else {
+        res.status(400).send('Usuario no existe');
+      }
+    }
+  });
+  connection.end();
+});
 
-
-//
-// Levanto el servidor WEB (pongo a escuchar)
-//
 app.listen(port, () => {
   console.log(`"server" Listening on port ${port}`);
 });
