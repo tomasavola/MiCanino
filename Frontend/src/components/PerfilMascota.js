2a75b57e46c48e2fcadf5098c7d69565c894b745
SANTINO LÁZZARI <46871386@ort>
2023-08-11 09:50:36 -0300

    avanbzamos perfil mascota

--- Frontend/src/components/PerfilMascota.js
+++ Frontend/src/components/PerfilMascota.js
@@ -1,78 +1,36 @@
-import NavBar from "./NavBar";
-import Logos from "./Logos";
-import FlechaVolver from './FlechaVolver';
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import Host from "./Host";
+import Host from './Host';
 
-function CargarCaninos() {
-    axios
-
-        .get('http://' + Host + ':5000/api/caninos')
-        .then((result) => {
-            const caninos = result.data.results;
-
-            caninos.map((canino, index) => {
-                const { nombre, url } = canino;
-                document.querySelector("#listado").innerHTML += `
-
-    <div class="col-3 pt-4; col">
-    <div class="services">
-    <div class="card graphic-design">
-      <div class="service-bg">
-      <br> <br>
-        <h5>${canino.nombre}</h5>
-        <br> 
-      </div>
-    </div>
-    </div>
-          `
-
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
-
-export default function PerfilMascota() {
-    const [canino, setCanino] = useState(null);
-    const { id } = useParams();
+function App() {
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        obtenerInformacionCaninos();
-    }, []);
-
-    const obtenerInformacionCaninos = async (id) => {
-        try {
-            const response = await axios.get(`http://A-PHZ2-CIDI-005:5000/api/caninos/${id}`);
-            setCanino(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+        // Utilizamos axios para realizar la solicitud a la API
+        axios.get(`http://${Host}:5000/api/caninos/`)
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error al obtener datos:', error);
+            });
+    }, []); // El array vacío asegura que useEffect se ejecute solo una vez al montar el componente
 
     return (
         <div>
-            <FlechaVolver />
-            <Logos />
-            <NavBar />
-
-            {canino ? (
-                <div>
-                    <h1 className="letraNegra">Información del Canino</h1>
-                    <p className="letraNegra">Nombre: {canino.nombre}</p>
-                    <p className="letraNegra">Fecha de Nacimiento: {canino.FechaNacimiento}</p>
-                    <p className="letraNegra">ID de Raza: {canino.idRaza}</p>
-                    <p className="letraNegra">Descripción: {canino.descripcion}</p>
-                    <p className="letraNegra">Peso: {canino.peso}</p>
-                </div>
-            ) : (
-                <p>Cargando información del canino...</p>
-            )}
+            <h1>Perfil Mascota</h1>
+            <ul>
+                {data.map(canino => (
+                    <li key={canino.id}>
+                        <strong>Raza:</strong> {canino.raza}<br />
+                        <strong>Fecha de Nacimiento:</strong> {canino.fecha}<br />
+                        <strong>Descripción:</strong> {canino.descripcion}<br />
+                        <strong>Peso:</strong> {canino.peso} kg
+                    </li>
+                ))}
+            </ul>
         </div>
-
     );
 }
 
+export default App;
