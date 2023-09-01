152fc3aaaba2d91910b44a2e176258a287f1b0bd
TOMAS AGUSTIN AVOLA <47297744@ort>
2023-09-01 10:40:33 -0300

    tratando de hacer registro

--- Frontend/src/components/FormularioRegistrarse.js
+++ Frontend/src/components/FormularioRegistrarse.js
@@ -1,55 +1,125 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function FormularioRegistro({ onAgregarCuenta }) {
-    const [idState, setId] = useState(0);
-    const [nombreState, setNombre] = useState('');
-    const [apellidoState, setApellido] = useState('');
-    const [mailState, setMail] = useState('');
-    const [telefonoState, setTelefono] = useState('');
-    const [contrasenaState, setContrasena] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [mail, setMail] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [contrasena, setContrasena] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function crearCuenta(event) {
-        event.preventDefault();
-        setId(idState + 1);
+  async function crearCuenta(event) {
+    event.preventDefault();
 
-        let cuenta = {
-            id: idState,
-            nombre: nombreState,
-            apellido: apellidoState,
-            mail: mailState,
-            telefono: telefonoState,
-            contrasena: contrasenaState
-        };
+    
 
-        onAgregarCuenta(cuenta);
-        agregarYNavegar();
+    const cuenta = {
+      nombre,
+      apellido,
+      mail,
+      telefono,
+      contrasena,
+    };
+
+try {
+      
+      const response = await fetch("http://A-PHZ2-CIDI-005:5000/api/usuario", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cuenta),
+      });
+
+      if (response.ok) {
+        navigate("/BrindaServicio");
+      } else {
+        console.error("Error al registrar el usuario");
+      }
+    } catch (error) {
+      console.error("Error de red:", error);
     }
 
-    function agregarYNavegar() {
-        navigate('/BrindaServicio');
-    }
+    onAgregarCuenta(cuenta);
 
-    return (
-        <>
-            <form onSubmit={crearCuenta}>
-                <center><h2 className="letraNegra">Información personal</h2></center>
-                <br></br>
-                <label className="letraNegra">Nombre</label>
-                <input type="text" name="nombre" className="controls" placeholder="Nombre" onChange={(e) => setNombre(e.target.value)} />
-                <label className="letraNegra">Apellido</label>
-                <input type="text" name="apellido" className="controls" placeholder="Apellido" onChange={(e) => setApellido(e.target.value)} />
-                <label className="letraNegra">Correo electrónico</label>
-                <input type="text" name="mail" className="controls" placeholder="Correo" onChange={(e) => setMail(e.target.value)} />
-                <label className="letraNegra">Telefono</label>
-                <input type="text" name="telefono" className="controls" placeholder="Telefono" onChange={(e) => setTelefono(e.target.value)} />
-                <label className="letraNegra">Contraseña</label>
-                <input type="text" name="Contrasena" className="controls" placeholder="Contraseña" onChange={(e) => setContrasena(e.target.value)} />
+    setNombre("");
+    setApellido("");
+    setMail("");
+    setTelefono("");
+    setContrasena("");
 
-                <button type="submit" className="botons">Crear cuenta</button>
-            </form>
-        </>
-    );
+    agregarYNavegar();
+  }
+
+  function agregarYNavegar() {
+    // Redirigir a la página de éxito o donde sea necesario
+    navigate("/BrindaServicio");
+  }
+
+  return (
+    <>
+      <form onSubmit={crearCuenta}>
+        <center>
+          <h2 className="letraNegra">Información personal</h2>
+        </center>
+        <br></br>
+        <label className="letraNegra">Nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          className="controls"
+          placeholder="Nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+          required
+        />
+        <label className="letraNegra">Apellido</label>
+        <input
+          type="text"
+          name="apellido"
+          className="controls"
+          placeholder="Apellido"
+          value={apellido}
+          onChange={(e) => setApellido(e.target.value)}
+          required
+        />
+        <label className="letraNegra">Correo electrónico</label>
+        <input
+          type="email"
+          name="mail"
+          className="controls"
+          placeholder="Correo"
+          value={mail}
+          onChange={(e) => setMail(e.target.value)}
+          required
+        />
+        <label className="letraNegra">Telefono</label>
+        <input
+          type="tel"
+          name="telefono"
+          className="controls"
+          placeholder="Telefono"
+          value={telefono}
+          onChange={(e) => setTelefono(e.target.value)}
+          required
+        />
+        <label className="letraNegra">Contraseña</label>
+        <input
+          type="password"
+          name="contrasena"
+          className="controls"
+          placeholder="Contraseña"
+          value={contrasena}
+          onChange={(e) => setContrasena(e.target.value)}
+          required
+        />
+
+        <button type="submit" className="botons">
+          Crear cuenta
+        </button>
+      </form>
+    </>
+  );
 }
