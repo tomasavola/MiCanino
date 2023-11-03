75e91efadfadd7a4d99115d42285a8f457d2635f
SANTINO LÁZZARI <46871386@ort>
2023-11-03 09:05:48 -0300

    avanzando historial

--- Frontend/src/components/HistorialMedicamentos.js
+++ Frontend/src/components/HistorialMedicamentos.js
@@ -8,57 +8,77 @@ import axios from 'axios';
 
 export default function HistorialMedicamentos() {
     const [medicamentos, setMedicamentos] = useState([]);
-    const [nombre, setNombre] = useState('');
+    const [medicamento, setMedicamento] = useState('');
     const [fecha, setFecha] = useState('');
     const [descripcion, setDescripcion] = useState('');
 
-    // Recuperar medicamentos almacenados en localStorage al cargar la página
     useEffect(() => {
-        const medicamentosGuardados = localStorage.getItem('medicamentos');
-        if (medicamentosGuardados) {
-            setMedicamentos(JSON.parse(medicamentosGuardados));
-        }
+        // Recuperar medicamentos desde la API al cargar la página
+        axios.get(`http://${Host}:5000/api/historial/`)
+            .then(response => {
+                setMedicamentos(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }, []);
 
     const agregarMedicamento = () => {
-
-        axios.post('http://' + Host + ':5000/api/historial/', )
-      .then(response => {
-        console.log(response.status);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-        if (nombre && fecha && descripcion) {
+        if (medicamento && fecha && descripcion) {
             const nuevoMedicamento = {
-                nombre,
+                medicamento,
                 fecha,
                 descripcion,
             };
-            const nuevosMedicamentos = [...medicamentos, nuevoMedicamento];
-            setMedicamentos(nuevosMedicamentos);
-            setNombre('');
+            // Enviar el nuevo medicamento a la API
+            axios.post(`http://${Host}:5000/api/historial?`, nuevoMedicamento)
+                .then(response => {
+                    console.log(response.status);
+                    // Recargar la lista de medicamentos desde la API
+                    axios.get(`http://${Host}:5000/api/historial/`)
+                        .then(response => {
+                            setMedicamentos(response.data);
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+            setMedicamento('');
             setFecha('');
             setDescripcion('');
-            // Almacenar los medicamentos en localStorage
-            localStorage.setItem('medicamentos', JSON.stringify(nuevosMedicamentos));
         }
     };
 
-    const eliminarMedicamento = (index) => {
-        const nuevosMedicamentos = medicamentos.filter((_, i) => i !== index);
-        setMedicamentos(nuevosMedicamentos);
-        // Actualizar medicamentos en localStorage
-        localStorage.setItem('medicamentos', JSON.stringify(nuevosMedicamentos));
+    const editarMedicamento = (id) => {
+        const medicamentoAEditar = medicamentos.find(m => m.id === id);
+        if (medicamentoAEditar) {
+            setMedicamento(medicamentoAEditar.medicamento);
+            setFecha(medicamentoAEditar.fecha);
+            setDescripcion(medicamentoAEditar.descripcion);
+        }
     };
 
-    const editarMedicamento = (index) => {
-        const medicamentoAEditar = medicamentos[index];
-        setNombre(medicamentoAEditar.nombre);
-        setFecha(medicamentoAEditar.fecha);
-        setDescripcion(medicamentoAEditar.descripcion);
-        eliminarMedicamento(index);
+    const eliminarMedicamento = (id) => {
+        // Eliminar el medicamento de la API
+        axios.delete(`http://${Host}:5000/api/historial/${id}`)
+            .then(response => {
+                console.log(response.status);
+                // Recargar la lista de medicamentos desde la API
+                axios.get(`http://${Host}:5000/api/historial`)
+                    .then(response => {
+                        setMedicamentos(response.data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     };
 
     const ordenarMedicamentosPorFecha = () => {
@@ -66,8 +86,6 @@ export default function HistorialMedicamentos() {
             return new Date(b.fecha) - new Date(a.fecha);
         });
         setMedicamentos(medicamentosOrdenados);
-        // Actualizar medicamentos ordenados en localStorage
-        localStorage.setItem('medicamentos', JSON.stringify(medicamentosOrdenados));
     };
 
     return (
@@ -78,16 +96,16 @@ export default function HistorialMedicamentos() {
             <h1 className="TituloMedicamento">Historial de Medicamentos</h1>
             <table>
                 <tbody>
-                    {medicamentos.map((medicamento, index) => (
-                        <tr key={index}>
-                            <td className="letraNegra">{medicamento.nombre}</td>
-                            <td className="letraNegra">{medicamento.fecha}</td>
-                            <td className="letraNegra">{medicamento.descripcion}</td>
+                    {medicamentos.map(med => (
+                        <tr key={med.id}>
+                            <td className="letraNegra">{med.medicamento}</td>
+                            <td className="letraNegra">{med.fecha}</td>
+                            <td className="letraNegra">{med.descripcion}</td>
                             <td className="actions">
-                                <button className='edit-button' onClick={() => editarMedicamento(index)}>
+                                <button className='edit-button' onClick={() => editarMedicamento(med.id)}>
                                     <RiEdit2Line className="edit-icon" size={17} />
                                 </button>
-                                <button className='delete-button' onClick={() => eliminarMedicamento(index)}>
+                                <button className='delete-button' onClick={() => eliminarMedicamento(med.id)}>
                                     <RiDeleteBin2Line className="delete-icon" size={17} />
                                 </button>
                             </td>
@@ -99,8 +117,8 @@ export default function HistorialMedicamentos() {
                 <input
                     type="text"
                     placeholder="Nombre"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
+                    value={medicamento}
+                    onChange={(e) => setMedicamento(e.target.value)}
                     className="controls"
                 />
                 <input
